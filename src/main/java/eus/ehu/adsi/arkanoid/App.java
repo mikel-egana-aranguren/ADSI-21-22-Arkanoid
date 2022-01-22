69b0633f2bc6344d0fe463b326d6756e1d49fbea
Joel <joelbraortiz@gmail.com>
2022-01-22 15:15:24 +0100

    Bugs solved

--- src/main/java/eus/ehu/adsi/arkanoid/App.java
+++ src/main/java/eus/ehu/adsi/arkanoid/App.java
@@ -1,14 +1,13 @@
 package eus.ehu.adsi.arkanoid;
 
-import eus.ehu.adsi.arkanoid.view.Personalizacion;
-import eus.ehu.adsi.arkanoid.view.SelectNivel;
 import eus.ehu.adsi.arkanoid.modelo.Arkanoid;
+import eus.ehu.adsi.arkanoid.view.SeleccionarNivel19;
 
 public class App {
 
 	public static void main(String[] args) {
 		//new Arkanoid(1).run();
-		new SelectNivel();
+		new SeleccionarNivel19();
 	}
 
 }
