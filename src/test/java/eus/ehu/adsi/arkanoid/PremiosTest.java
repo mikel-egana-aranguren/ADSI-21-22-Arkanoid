49e3704f356925cc866bd7da3e16ad15b432b694
anegda <anegdaldama@gmail.com>
2022-01-23 15:08:25 +0100

    Cambio de nombre de clase

--- src/test/java/eus/ehu/adsi/arkanoid/PremiosTest.java
+++ src/test/java/eus/ehu/adsi/arkanoid/PremiosTest.java
@@ -10,22 +10,22 @@ import eus.ehu.adsi.arkanoid.core.Game;
 import eus.ehu.adsi.arkanoid.view.Ball;
 import eus.ehu.adsi.arkanoid.view.Brick;
 
-public class Premios {
+public class PremiosTest {
 	
-	private static final Logger logger = LogManager.getLogger(Premios.class);
+	private static final Logger logger = LogManager.getLogger(PremiosTest.class);
 
 	@Test
-	public void prueba1A() {
+	public void prueba0A() {
 		
 	}
 	
 	@Test
-	public void prueba1B() {
+	public void prueba0B() {
 
 	}
 	
 	@Test
-	public void prueba2A() {
+	public void prueba1A() {
 		//Para realizar esta prueba miramos los resultados que nos da el método obtenerPremiosObtenidos un usuario sin premios
 		String si = Arkanoid.obtenerPremiosObtenidos("urkazio");
 		assertTrue(si.length()==0);
@@ -35,7 +35,7 @@ public class Premios {
 	}
 	
 	@Test
-	public void prueba2B() {
+	public void prueba1B() {
 		//Para realizar esta prueba miramos los resultados que nos da el método  obtenerPremiosObtenidos un usuario con todos los premios
 		String si = Arkanoid.obtenerPremiosObtenidos("endika1");
 		assertTrue(si.length()!=0);
@@ -44,9 +44,11 @@ public class Premios {
 		assertTrue(no.length()==0);
 	}
 	
+	//Las pruebas que se proponen 2A y 2B no ocurren => No hago los casos de prueba.
+	 
 	@Test
 	public void prueba3A() {
-
+		//
 	}
 	
 	@Test
